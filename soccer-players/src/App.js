17727d5da0707bf03bc59aa1dedf58333bee6729
Tommy Tomaye <tommytomaye2018@gmail.com>
2020-12-24 21:00:06 -0800

    SoccerPlayersTable component added

--- soccer-players/src/App.js
+++ soccer-players/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import SoccerPlayersTable from './soccer-players-table';
 
 function App() {
   return (
     <div className="App">
       <h1>Soccer players App</h1>
+      <SoccerPlayersTable />
     </div>
   );
 }
---
+++ soccer-players/src/soccer-players-table.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class SoccerPlayersTable extends Component {
+    render() {
+        return(
+          <p>Soccer players table.</p>
+        )
+      }
+}
