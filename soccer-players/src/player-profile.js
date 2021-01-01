02fe3322e13f724f2c31280c6cb74de475ecbe90
Tommy Tomaye <tommytomaye2018@gmail.com>
2020-12-31 20:22:07 -0800

    removing extra code

--- soccer-players/src/player-profile.js
+++ soccer-players/src/player-profile.js
@@ -4,6 +4,21 @@ import {Link} from 'react-router-dom';
 import './player-profile.css';
 
 class PlayerProfile extends Component {
+
+  renderPlayerInfo () {
+    return (
+      <div>
+        {Object.keys(playerInfo[this.props.match.params.id]).map((val, index) => {
+          if (val !== 'id' && val !== 'picture') {
+              return <div className="d-flex" key={index}><div className="col-4"><strong>{val.toLocaleUpperCase()}:</strong></div><div className="col"><span>{playerInfo[this.props.match.params.id][val]}</span></div></div>
+          } else {
+            return ''
+          }
+        })}
+      </div>
+    )
+  }
+
   render() {
     return  <div className="card d-flex justify-content-center">
     <div className="card-body">
@@ -11,19 +26,9 @@ class PlayerProfile extends Component {
       <h6 className="card-title">Player profile</h6>
       </div>
       <div className="col-12 d-flex">
-        <div className="col-4"><img width="250" src={playerInfo[this.props.match.params.id].picture}/></div>
+        <div className="col-4"><img width="250" src={playerInfo[this.props.match.params.id].picture} alt="player" /></div>
         <div className="col-8" id="info-section">
-            <div className="d-flex"><div className="col-3"><strong>Name:</strong></div><div className="col"><span>{playerInfo[this.props.match.params.id].name}</span></div></div>
-            <div className="d-flex"><div className="col-3"><strong>Number:</strong></div><div className="col"><span>{playerInfo[this.props.match.params.id].number}</span></div></div>
-            <div className="d-flex"><div className="col-3"><strong>Birthday:</strong></div><div className="col"><span>{playerInfo[this.props.match.params.id].birthday}</span></div></div>
-            <div className="d-flex"><div className="col-3"><strong>Weight:</strong></div><div className="col"><span>{playerInfo[this.props.match.params.id].weight}</span></div></div>
-            <div className="d-flex"><div className="col-3"><strong>Playing position(s):</strong></div><div className="col"><span>{playerInfo[this.props.match.params.id]['Playing position(s)']}</span></div></div>
-            <div className="d-flex"><div className="col-3"><strong>Height:</strong></div><div className="col"><span>{playerInfo[this.props.match.params.id].height}</span></div></div>
-            <div className="d-flex"><div className="col-3"><strong>Age:</strong></div><div className="col"><span>{playerInfo[this.props.match.params.id].age}</span></div></div>
-            <div className="d-flex"><div className="col-3"><strong>Country:</strong></div><div className="col"><span>{playerInfo[this.props.match.params.id].country}</span></div></div>
-            <div className="d-flex"><div className="col-3"><strong>Goals:</strong></div><div className="col"><span>{playerInfo[this.props.match.params.id].goals}</span></div></div>
-            <div className="d-flex"><div className="col-3"><strong>Team:</strong></div><div className="col"><span>{playerInfo[this.props.match.params.id].team}</span></div></div>
-            <div className="d-flex"><div className="col-3"><strong>Salary:</strong></div><div className="col"><span>{playerInfo[this.props.match.params.id].salary}</span></div></div>
+            {this.renderPlayerInfo()}
             <div className="d-flex col mt-5">
             <Link to="/"><button className="btn btn-primary">Back to Players List</button></Link>
             </div>
